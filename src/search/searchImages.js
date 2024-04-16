808532f3c7b92b1d6799bf04f133448bc09e5651
yosoySR <155894514+SebastianRodriguezD@users.noreply.github.com>
2024-04-16 18:04:20 +0200

    Update searchImages.js

--- src/search/searchImages.js
+++ src/search/searchImages.js
@@ -4,9 +4,16 @@ export async function searchImages(query, page, perPage) {
       `https://api.unsplash.com/search/photos?query=${query}&per_page=${perPage}&page=${page}&client_id=MVKwbul345zYqwXUH2Uc_8Zwd5vQ5RiYB7RlB4fxGEk`
     )
     const data = await response.json()
+    if (data.results.length === 0) {
+      mostrarMensaje('No se encontraron imágenes para ' + query)
+    }
     return data.results
   } catch (error) {
     console.error('Error fetching images for query:', error)
     return []
   }
+}
+
+function mostrarMensaje(mensaje) {
+  alert(mensaje)
 }
